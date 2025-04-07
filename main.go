d533a41bb70aa09cb85f66cccb10d231dc3dd4a5
Diego Lara <diegolara93345@gmail.com>
2025-04-07 14:45:21 -0500

    POST request

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -17,44 +18,80 @@ func handleConnection(conn net.Conn) {
 			fmt.Println("error reading from connection")
 			os.Exit(1)
 		}
-		if line == "\r\n" {
+		reqs = append(reqs, line)
+		if line == "\r\n" && strings.Split(reqs[0], " ")[0] == "POST" {
+			contentLength := reqs[2]
+			s := strings.Split(contentLength, " ")
+			contentLengthInt, err := strconv.Atoi(strings.TrimSpace(s[1]))
+			if err != nil {
+				fmt.Println("error converting to int")
+			}
+			b, _ := reader.Peek(contentLengthInt)
+			reqs = append(reqs, string(b[:]))
+			break
+		} else if line == "\r\n" {
 			break
 		}
-		reqs = append(reqs, line)
 	}
 	getRequest := reqs[0]
 	getRequestArr := strings.Split(getRequest, " ")
-	if getRequestArr[1] == "/" {
-		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-	} else if getRequestArr[1][0:6] == "/echo/" {
-		contentLength := len(getRequestArr[1][6:])
-		content := getRequestArr[1][6:]
-		resp := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %v\r\n\r\n%v", contentLength, content)
-		conn.Write([]byte(resp))
-	} else if getRequestArr[1] == "/user-agent" {
-		agentLine := strings.Split(reqs[2], " ")
-		contentLength := len(strings.TrimSpace(agentLine[1]))
-		agent := agentLine[1]
-		resp := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %v\r\n\r\n%v", contentLength, agent)
-		conn.Write([]byte(resp))
-
-	} else if getRequestArr[1][0:7] == "/files/" {
-		file := getRequestArr[1][7:]
-		path := os.Args[2]
-		fmt.Printf(path)
-		os.Chdir(path)
-		contents, err := os.ReadFile(file)
-		if err != nil {
-			fmt.Println("error reading file")
-			conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
-			fmt.Println(file)
-		} else {
-			contentLength := len(string(contents))
-			resp := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: %v\r\n\r\n%v", contentLength, string(contents))
+	for _, v := range reqs {
+		fmt.Print("------->")
+		fmt.Printf(v)
+		fmt.Println()
+	}
+	if getRequestArr[0] == "GET" {
+		if getRequestArr[1] == "/" {
+			conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+		} else if getRequestArr[1][0:6] == "/echo/" {
+			contentLength := len(getRequestArr[1][6:])
+			content := getRequestArr[1][6:]
+			resp := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %v\r\n\r\n%v", contentLength, content)
 			conn.Write([]byte(resp))
+		} else if getRequestArr[1] == "/user-agent" {
+			agentLine := strings.Split(reqs[2], " ")
+			contentLength := len(strings.TrimSpace(agentLine[1]))
+			agent := agentLine[1]
+			resp := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %v\r\n\r\n%v", contentLength, agent)
+			conn.Write([]byte(resp))
+
+		} else if getRequestArr[1][0:7] == "/files/" {
+			file := getRequestArr[1][7:]
+			path := os.Args[2]
+			fmt.Printf(path)
+			os.Chdir(path)
+			contents, err := os.ReadFile(file)
+			if err != nil {
+				fmt.Println("error reading file")
+				conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+				fmt.Println(file)
+			} else {
+				contentLength := len(string(contents))
+				resp := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: %v\r\n\r\n%v", contentLength, string(contents))
+				conn.Write([]byte(resp))
+			}
+		} else {
+			conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
 		}
-	} else {
-		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+	} else if getRequestArr[0] == "POST" {
+		// Implement POST logic here
+		if getRequestArr[1][0:7] == "/files/" {
+			fileName := getRequestArr[1][7:]
+			path := os.Args[2]
+			fmt.Printf(path)
+			os.Chdir(path)
+			file, err := os.Create(fileName)
+			file.WriteString(reqs[len(reqs)-1])
+			// contents, err := os.ReadFile(file)
+			if err != nil {
+				fmt.Println("error creating file")
+				conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+				fmt.Println(fileName)
+			} else {
+				resp := fmt.Sprintf("HTTP/1.1 201 Created\r\n\r\n")
+				conn.Write([]byte(resp))
+			}
+		}
 	}
 }
 
